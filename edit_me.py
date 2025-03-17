1a291eb43ef51f28a44d0b060f436ee75aae1f70
JasmineChap7 <jgray014@drury.edu>
2025-03-17 16:45:17 -0500

    final lab 2

--- edit_me.py
+++ edit_me.py
@@ -1,18 +1,12 @@
-def search(lst, key):
+def monotonic(lst):
+    inc = dec = True
+    for i in range(1, len(lst) - 1):
+        if lst[i] > lst[i+1]:
+            inc = False
+        elif lst[i] < lst[i+1]:
+            dec = False
+    return inc or dec
 
-    assert monotonic(lst), "The list is not sorted"
-
-    low = 0
-    mid = len(lst) // 2
-    high = len(lst) - 1
-
-    while (high >= low):
-        mid = (high + low) // 2
-        if (lst[mid] < key):
-          low = mid + 1
-        elif (lst[mid] > key):
-            high = mid - 1
-        else:
-            return mid
-
-    return -1
+assert monotonic([1,3,4,100]) == True
+assert monotonic([1,2,1,2]) == False
+assert monotonic([100, 56, 3, 2, 2, 1]) == True
